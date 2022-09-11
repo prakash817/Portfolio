795ebe4ae7bb4f7e075f1c7b6ab5165ac60c4d8a
Prakash Gupta <96722484+prakash817@users.noreply.github.com>
2022-09-11 12:09:51 +0530

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -97,8 +97,8 @@ const Home = () => {
                <div className="mt-3 sm:mt-4 lg:mt-10 cursor-pointer w-1/2">
                 <a
                   className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
-                  href="Prakash gupta_FW16_049.pdf"
-                  
+                  href="Prakash gupta_FW16_049.pdf" 
+                  target="_blank"
                 
                 >
                   Download Resume
